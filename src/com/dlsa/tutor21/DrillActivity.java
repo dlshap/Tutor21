f7465953a9404bab0acbad0a9d28ac4b42d2b55a
David Shapiro <DavidS@dlsa.com>
2013-08-25 14:44:10 -0500

    Fixed it!

--- src/com/dlsa/tutor21/DrillActivity.java
+++ src/com/dlsa/tutor21/DrillActivity.java
@@ -16,16 +16,16 @@ public class DrillActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_drill);
 		
-		Card dCard = new Card(3,1); // 3 of Spades
-		Card pCard1 = new Card(1,4); // Ace of Clubs
-		Card pCard2 = new Card(13,2); //King of Hearts
-		
-		tvDCard = (TextView) findViewById(R.id.txtDealerCard1);
-		tvDCard.setText(dCard.toString());
-		tvPCard1 = (TextView) findViewById(R.id.txtPlayerCard1);
-		tvPCard1.setText(pCard1.toString());
-		tvPCard2 = (TextView) findViewById(R.id.txtPlayerCard2);
-		tvPCard2.setText(pCard2.toString());
+//		Card dCard = new Card(3,1); // 3 of Spades
+//		Card pCard1 = new Card(1,4); // Ace of Clubs
+//		Card pCard2 = new Card(13,2); //King of Hearts
+//		
+//		tvDCard = (TextView) findViewById(R.id.txtDealerCard1);
+//		tvDCard.setText(dCard.toString());
+//		tvPCard1 = (TextView) findViewById(R.id.txtPlayerCard1);
+//		tvPCard1.setText(pCard1.toString());
+//		tvPCard2 = (TextView) findViewById(R.id.txtPlayerCard2);
+//		tvPCard2.setText(pCard2.toString());
 
 	}
 
@@ -37,7 +37,7 @@ public class DrillActivity extends Activity {
 		final Button doubleButton = (Button) findViewById(R.id.btnDeal);
 		doubleButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				tvPCard2.setText("You hit the Double button!");
+//				tvPCard2.setText("You hit the Double button!");
 			}
 		});
 		
